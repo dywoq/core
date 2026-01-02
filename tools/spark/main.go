7cff41de5d2be20b35076b9ca4141f3bd19aaee7
dywoq <aleks.koyf@gmail.com>
2026-01-02 16:08:39 +0300

    tools/spark: Remove redudant type *cobra.Command

--- tools/spark/main.go
+++ tools/spark/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd *cobra.Command = &cobra.Command{
+var rootCmd = &cobra.Command{
 	Use:   "spark",
 	Short: "spark is a test executor, specifically made for dywoq/cxx.",
 	Long: `spark is a test executor written in Go,
