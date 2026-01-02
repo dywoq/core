36c1f65c52f5e5586ebac3cb7f0f7f3526a47aec
dywoq <aleks.koyf@gmail.com>
2026-01-02 16:08:19 +0300

    tools/spark: Add root command

--- tools/spark/main.go
+++ tools/spark/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/spf13/cobra"
+)
+
+var rootCmd *cobra.Command = &cobra.Command{
+	Use:   "spark",
+	Short: "spark is a test executor, specifically made for dywoq/cxx.",
+	Long: `spark is a test executor written in Go,
+which is specifically made for C++ standard library dywoq/cxx.
+Repository: github.com/dywoq/cxx/tools/spark`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to run command: %v", err)
+		os.Exit(1)
+	}
 }
